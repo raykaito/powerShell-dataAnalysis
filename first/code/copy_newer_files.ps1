897b8f4901971a5d4b1fb6537fb2ae74d212c21c
raykaito <kazukykaito@gmail.com>
2022-04-19 07:05:37 +0900

    copy_newer_files_update
    An update was made to LIST the input and output directory so files in multiple folders can be copied at once.

--- first/code/copy_newer_files.ps1
+++ first/code/copy_newer_files.ps1
@@ -1,56 +1,73 @@
 ﻿#Define source and destination directory (RESULTS\OK)
-$pathResultOkSrc = Convert-Path "$($PSScriptRoot)\..\data\raw"
-$pathResultOkDest = Convert-Path "$($PSScriptRoot)\..\data\raw_copy"
+$copyFromList = @(
+    "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\raw\div1",
+    "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\raw\div2",
+    "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\raw\div3"
+)
+$copyToList = @(
+    "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\raw_copy\div1",
+    "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\raw_copy\div2",
+    "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\raw_copy\div3"
+)
 
-#Get month list in source and Dest
-$monthListSrc = Get-ChildItem -Path $pathResultOkSrc -Directory
-$monthListDest = Get-ChildItem -Path $pathResultOkDest -Directory
+Function copy_newer_items ($pathResultOkSrc, $pathResultOkDest){
 
-if($monthListDest.Length -gt 0){
-    #Get latest folder in Dest
-    $latestMonthName = $monthListDest.Name | measure -Maximum
-    $latestMonthName = $latestMonthName.Maximum
-    $latestMonthDir = "$($pathResultOkDest)\$latestMonthName\"
+    #Get month list in source and Dest
+    $monthListSrc = Get-ChildItem -Path $pathResultOkSrc -Directory
+    $monthListDest = Get-ChildItem -Path $pathResultOkDest -Directory
 
-    #Get file list in srouce(latest month)
-    $fileListSrc = Get-ChildItem -Path "$($pathResultOkSrc)\$latestMonthName"
+    if($monthListDest.Length -gt 0){
+        #Get latest folder in Dest
+        $latestMonthName = $monthListDest.Name | measure -Maximum
+        $latestMonthName = $latestMonthName.Maximum
+        $latestMonthDir = "$($pathResultOkDest)\$latestMonthName\"
 
-    #In latest month dir, get latest file
-    $fileListDest = Get-ChildItem -Path "$($pathResultOkDest)\$latestMonthName"
-    $fileListDestInt = @()
-    foreach ($file in $fileListDest) {
-        $fileListDestInt += $file.Name -replace "[^0-9]" , ''
-    }
-    $latestFileName = $fileListDestInt | measure -Maximum
-    $latestFileName = $latestFileName.Maximum
+        #Get file list in srouce(latest month)
+        $fileListSrc = Get-ChildItem -Path "$($pathResultOkSrc)\$latestMonthName"
 
-    #Copy newer files
-    foreach($fileSrc in $fileListSrc){
-        $currentFileName = $fileSrc.Name -replace "[^0-9]" , ''
-        if($latestFileName -gt $currentFileName){
-            continue
+        #In latest month dir, get latest file
+        $fileListDest = Get-ChildItem -Path "$($pathResultOkDest)\$latestMonthName"
+        $fileListDestInt = @()
+        foreach ($file in $fileListDest) {
+            $fileListDestInt += $file.Name -replace "[^0-9]" , ''
         }
-        Copy-Item -Path $fileSrc.FullName -Destination $latestMonthDir
-        write("Copying...  $latestMonthName\$fileSrc")
+        $latestFileName = $fileListDestInt | measure -Maximum
+        $latestFileName = $latestFileName.Maximum
+
+        #Copy newer files
+        foreach($fileSrc in $fileListSrc){
+            $currentFileName = $fileSrc.Name -replace "[^0-9]" , ''
+            if($latestFileName -gt $currentFileName){
+                continue
+            }
+            Copy-Item -Path $fileSrc.FullName -Destination $latestMonthDir
+            write("Copying...  $latestMonthName\$fileSrc")
+        }
+
+        #Copy every file in newer month dir
+        foreach($monthDirSrc in $monthListSrc){
+            if($latestMonthName -ge $monthDirSrc.Name){
+                continue
+            }
+            $newMonthDir = "$($pathResultOkDest)\$($monthDirSrc.Name)\"
+            New-Item $newMonthDir -ItemType Directory
+            $newMonthDir = $newMonthDir
+            Copy-Item -Path "$($monthDirSrc.FullName)\*" -Destination $newMonthDir -Recurse
+        }
+    }else{
+        #Copy every folder
+        foreach($monthDirSrc in $monthListSrc){
+            $newMonthDir = "$($pathResultOkDest)\$($monthDirSrc.Name)\"
+            New-Item $newMonthDir -ItemType Directory
+            Copy-Item -Path "$($monthDirSrc.FullName)\*" -Destination $newMonthDir -Recurse
+        }
     }
 
-    #Copy every file in newer month dir
-    foreach($monthDirSrc in $monthListSrc){
-        if($latestMonthName -ge $monthDirSrc.Name){
-            continue
-        }
-        $newMonthDir = "$($pathResultOkDest)\$($monthDirSrc.Name)\"
-        New-Item $newMonthDir -ItemType Directory
-        $newMonthDir = $newMonthDir
-        Copy-Item -Path "$($monthDirSrc.FullName)\*" -Destination $newMonthDir -Recurse
-    }
-}else{
-    #Copy every folder
-    foreach($monthDirSrc in $monthListSrc){
-        $newMonthDir = "$($pathResultOkDest)\$($monthDirSrc.Name)\"
-        New-Item $newMonthDir -ItemType Directory
-        Copy-Item -Path "$($monthDirSrc.FullName)\*" -Destination $newMonthDir -Recurse
-    }
 }
 
+Foreach($index in 0..($copyFromList.Length-1)){
+    $PathSrc = Convert-Path $copyFromList[$index]
+    $PathDest= Convert-Path $copyToList[$index]
+    copy_newer_items $PathSrc $PathDest
+}
 Read-Host -Prompt "Press Enter to Exit"
---
+++ first/code/graph_test.ps1
@@ -0,0 +1,48 @@
+﻿Function draw($graphName){
+    #DefineParameters
+    $width = 720
+    $height= 480
+    $plot_x_0 = 60
+    $plot_x_1 = 670
+    $plot_y_0 = 420
+    $plot_y_1 = 50
+
+    #Prepare Graph
+    Add-Type -AssemblyName System.Drawing
+    $bmp = new-object System.Drawing.Bitmap $width,$height 
+    $graphics = [System.Drawing.Graphics]::FromImage($bmp)
+
+    #Fill Background
+    $backGround = [System.Drawing.Brushes]::gray 
+    $graphics.FillRectangle($backGround,0,0,$bmp.Width,$bmp.Height) 
+
+    #Draw Axis
+    $Pen = [Drawing.Pens]::Black
+    $graphics.DrawLine($Pen,$plot_x_0,$plot_y_0,$plot_x_1,$plot_y_0) 
+    $graphics.DrawLine($Pen,$plot_x_0,$plot_y_0,$plot_x_0,$plot_y_1)
+    
+    #Draw plots
+    $BrushBlue = [System.Drawing.Brushes]::Blue 
+    $graphics.FillEllipse($BrushBlue,101,210,3,3)
+
+    #Draw Strings
+    $fontColor = [System.Drawing.Brushes]::Black  
+    $font = new-object System.Drawing.Font Arial, 12
+    $graphics.DrawString('My Graph',$font,$fontColor,10,10)
+
+    #Draw xyguide
+    $StringFormat = [System.Drawing.StringFormat]::New()
+    $StringFormat.Alignment = "Center"
+    $StringFormat.LineAlignment = "Center"
+    $graphics.DrawString("Here",$font,$fontColor,$plot_x_0,$plot_y_0+12,$StringFormat)
+
+    $graphics.Dispose() 
+    $bmp.Save($graphName) 
+
+    Invoke-Item "$($graphName)"
+}
+$pathRawData = "$($PSScriptRoot)\..\data\raw"
+
+draw "C:\Users\Kazuk\Documents\GitHub\powerShell-dataAnalysis\first\data\firstGraph.png"
+
+#Read-Host -Prompt "Press Enter to Exit"
